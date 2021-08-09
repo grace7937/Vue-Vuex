01a2ac67bda41c0b043e46554d4bd1b3bd1c998b
grace7937 <eunchong.k153@gmail.com>
2021-08-09 23:23:39 +0900

    mehtods

---
+++ src/components/UserList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      User TodoList
+      <div v-for="user in users" :key="user.id">
+          {{ user.name}}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { mapState, mapActions } from 'vuex';
+export default {
+  created() {
+    this.getusers();
+  },
+  computed: {
+      ...mapState([
+          'users',
+          'todos'
+      ]),
+    users() {
+      return this.$store.state.users;
+    },
+    todos() {
+        return this.$store.state.todos;
+    }
+  },
+  methods: {
+      ...mapActions([
+          'getUsers'
+      ])
+    getUsers() {
+      this.$store.dispatch("getUsers");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ test.java
@@ -0,0 +1,43 @@
+/**
+ * test
+ */
+public class test {
+
+    public int checkNum() {
+        int num;
+        num 
+        return 
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+public class Profile {
+    public static void name(String[] args) {
+        String name = "kim";
+        int age = "40";
+        System.out.printIn("my name is" + name);
+        System.out.printIn("I'm" + age);
+    }
+}
+
+public class Calculator {
+    public int subtract(int num1, int num2) {
+        int sum;
+        sum = num1 + num2;  
+        return sum;
+    }
+    public int multiply(int num1, int num2) {
+        int sum;
+        sum = num1 * num2;  
+        return sum;
+    }
+    public int divide(int num1, int num2) {
+        int sum;
+        sum = num1 / num2;  
+        return sum;
+    }
+}
